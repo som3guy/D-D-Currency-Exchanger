7e6ab4979c2f6c245b97cfa5afe61c5aa6d5471c
Jeremy Miller <jmiller3346@gmail.com>
2018-09-04 09:15:26 -0500

    Initial Commit

---
+++ D&D Currency Exchanger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D&D Currency Exchanger
+//
+//  Created by Miller,Jeremy J on 9/4/18.
+//  Copyright © 2018 som3guy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ D&D Currency ExchangerTests/D_D_Currency_ExchangerTests.swift
@@ -0,0 +1,36 @@
+//
+//  D_D_Currency_ExchangerTests.swift
+//  D&D Currency ExchangerTests
+//
+//  Created by Miller,Jeremy J on 9/4/18.
+//  Copyright © 2018 som3guy. All rights reserved.
+//
+
+import XCTest
+@testable import D_D_Currency_Exchanger
+
+class D_D_Currency_ExchangerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
