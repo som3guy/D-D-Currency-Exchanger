4e27f4e05728069df24d3bfad253b09a6fac06ef
Jeremy Miller <jeremy.j.miller@cerner.com>
2019-05-09 13:52:37 -0500

    initial commit

--- D&D Currency Exchanger/ViewController.swift
+++ D&D Currency Exchanger/ViewController.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
+class FormViewController {
+    // Create form instace
+    var form = FormDescriptor()
+    form.title = "Example form"
+    
+    // Define first section
+    var section1 = FormSectionDescriptor()
+    
+    var row = FormRowDescriptor(tag: "name", rowType: .Email, title: "Email")
+    section1.rows.append(row)
+    
+    row = FormRowDescriptor(tag: "pass", rowType: .Password, title: "Password")
+    section1.rows.append(row)
+    
+    // Define second section
+    var section2 = FormSectionDescriptor()
+    
+    row = FormRowDescriptor(tag: "button", rowType: .Button, title: "Submit")
+    section2.rows.append(row)
+    
+    form.sections = [section1, section2]
+    
+    self.form = form
+}
+
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
 
